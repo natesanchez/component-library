7cad1b38a512b2ec0d2cf9568246a9a643cc50c1
Nate_Sanchez <NSanchez@usa.com>
2020-02-23 18:42:38 -0500

    Form redo and test

---
+++ src/Forms/Forms.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import "./Forms.css"
+
+class Forms extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+
+        };
+    }
+
+
+
+    render() {  
+      return (
+      <h1>test</h1>
+      )
+    }
+  }
+
+
+
+export default Forms;
---
+++ src/Forms/Forms.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Forms from './Forms';
+
+
+storiesOf('Forms', module)
+    .add('test', () => <Forms />)
