80bc59aff6f3df93c83e606a46d3c1755fecff30
Nate_Sanchez <NSanchez@usa.com>
2020-02-22 23:11:15 -0500

    buttons

--- src/Buttons/Button.js
+++ src/Buttons/Button.js
@@ -4,21 +4,31 @@ import "./Button.css"
 
 const Button = (props) => {
     let classList = '';
-    let types = ['primary', 'danger', 'success', 'warning', 'white', 'gradient']
+    let types = ['primary', 'danger', 'success', 'warning',]
     if (types.includes(props.type)) {
         classList += ` button-${props.type}`
     }
     if (props.large) {
-        classList += `button-large`
+        classList += ` button-large`
+    }
+
+    if (props.whiteText) {
+        classList += ` button-whiteText`
     }
 
     if (props.white) {
-        classList += `button-white`
+        classList += ` button-white`
     }
 
+    if (props.gradient) {
+        classList += ` button-${props.type}-gradient`
+    }  
+    
+    if (props.defaultGradient) {
+        classList += ` button-default-gradient`
+    } 
 
     return <button className={classList}>{props.label}</button>
 }
-
 
 export default Button; 
--- src/Buttons/Button.stories.js
+++ src/Buttons/Button.stories.js
@@ -8,16 +8,159 @@ storiesOf('Button', module)
     .add('Primary', () => <Button 
     label="Do Something"
     type="primary" 
+    whiteText
     />)
+    .add('Primary-White', () => <Button 
+    label="Do Something"
+    type="primary" 
+    white
+    />)
+    .add('Primary-Gradient', () => <Button 
+    label="Do Something"
+    type="primary" 
+    gradient
+    />)        
+    .add('Primary-Large', () => <Button 
+    label="Do Something"
+    type="primary" 
+    whiteText
+    large
+    />)
+    .add('Primary-Large-Gradient', () => <Button 
+    label="Do Something"
+    type="primary" 
+    gradient
+    large
+    />)
+    .add('Primary-Large-White', () => <Button 
+    label="Do Something"
+    type="primary" 
+    white
+    large
+    />)                  
     .add('Danger', () => <Button
     label="Do Something"
     type="danger"
+    whiteText
     />)
+    .add('Danger-White', () => <Button 
+    label="Do Something"
+    type="danger" 
+    white
+    />)
+    .add('Danger-Gradient', () => <Button 
+    label="Do Something"
+    type="danger" 
+    gradient
+    />)            
+    .add('Danger-Large', () => <Button 
+    label="Do Something"
+    type="danger" 
+    whiteText
+    large
+    />)
+    .add('Danger-Large-Gradient', () => <Button 
+    label="Do Something"
+    type="danger" 
+    gradient
+    large
+    />)
+    .add('Danger-Large-White', () => <Button 
+    label="Do Something"
+    type="danger" 
+    white
+    large
+    />)            
     .add("Success", () => <Button
     label="Do Something"
     type= "success"
-    />)   
+    whiteText
+    />)
+    .add('Success-White', () => <Button 
+    label="Do Something"
+    type="success" 
+    white
+    />)
+    .add('Success-Gradient', () => <Button 
+    label="Do Something"
+    type="success" 
+    gradient
+    />)            
+    .add('Success-Large', () => <Button 
+    label="Do Something"
+    type="success" 
+    whiteText
+    large
+    />)
+    .add('Success-Large-Gradient', () => <Button 
+    label="Do Something"
+    type="success" 
+    gradient
+    large
+    />)
+    .add('Success-Large-White', () => <Button 
+    label="Do Something"
+    type="success" 
+    white
+    large
+    />)                
     .add('Warning', () => <Button 
     label="Do Something" 
     type="warning"
-    />) 
+    whiteText        
+    />)
+    .add('Warning-White', () => <Button 
+    label="Do Something"
+    type="warning" 
+    white
+    />)
+    .add('Warning-Gradient', () => <Button 
+    label="Do Something"
+    type="warning" 
+    gradient
+    />)            
+    .add('Warning-Large', () => <Button 
+    label="Do Something"
+    type="warning" 
+    whiteText
+    large
+    />)
+    .add('Warning-Large-Gradient', () => <Button 
+    label="Do Something"
+    type="warning" 
+    gradient
+    large
+    />)
+    .add('Warning-Large-White', () => <Button 
+    label="Do Something"
+    type="warning" 
+    white
+    large
+    />)               
+    .add('Default', () => <Button 
+    label="Do Something"
+    whiteText
+    />)
+    .add('Default-White', () => <Button 
+    label="Do Something"
+    white
+    />)
+    .add('Default-Gradient', () => <Button 
+    label="Do Something"
+    defaultGradient
+    />)            
+    .add('Default-Large', () => <Button 
+    label="Do Something"
+    whiteText
+    large
+    />)
+     .add('Default-Large-Gradient', () => <Button 
+     label="Do Something"
+     defaultGradient
+     large
+     />)
+    .add('Default-Large-White', () => <Button 
+    label="Do Something"
+    white
+    large
+    />)               
