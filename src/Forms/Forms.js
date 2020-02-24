a78fe4aa9ce5dc38fdedcd866534c4dddc8ff500
Nate_Sanchez <NSanchez@usa.com>
2020-02-23 22:40:26 -0500

    counter functionality and extra style

--- src/Forms/Forms.js
+++ src/Forms/Forms.js
@@ -9,16 +9,24 @@ class Forms extends React.Component {
         counter: 0
         };
     }
+
+    counterUp = () => {
+      this.setState({
+        counter: this.state.counter + 1
+      })
+    }
+
+    counterDown = () => {
+      this.setState({
+        counter: this.state.counter - 1
+      })
+    }
+
     render() {  
       let classes = '';
       let types = ['text', 'select', 'number', 'textbutton', 'checkbox']
       if (types.includes(this.props.type)) {
           classes += ` form-${this.props.type}`
-      }
-
-      let placeholder = this.props.placeholder
-      if (placeholder === "") {
-          placeholder = this.props.label
       }
 
       if(this.props.medium) {
@@ -31,6 +39,11 @@ class Forms extends React.Component {
 
       if(this.props.fill) {
         classes += ` selectFill`
+      }
+
+      let placeholder = this.props.placeholder
+      if (placeholder === "") {
+          placeholder = this.props.label
       }
 
     if (this.props.text) {
@@ -60,7 +73,9 @@ class Forms extends React.Component {
     if(this.props.number) {
       return (
           <div className={classes}>
-            <div className="numberClick">-</div><span>{this.state.counter}</span><div className="numberClick">+</div>
+              <div className="numberClick" onClick={this.counterDown}>-</div>
+              <span>{this.state.counter}</span>
+              <div className="numberClick" onClick={this.counterUp}>+</div>
           </div>
       )
   }  
